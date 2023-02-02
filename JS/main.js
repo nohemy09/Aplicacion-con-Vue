bc2d50f40da97e80d2f7b8fa937eb8a0f739c10d
nemo2109 <114691858+nohemy09@users.noreply.github.com>
2023-02-02 17:42:20 -0600

    App con vue creada

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,33 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// Tenemos que crear la aplicacion
+Vue.createApp({
+  // Tenemos que definir la data
+  data() {
+    return {
+      goals: [],
+      enteredValue: '', // Conectralo con HTML
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    } // El metodo para agregar goals
+  }
+}).mount('#app');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
 
-buttonEl.addEventListener('click', addGoal);
- 40  
+//const buttonEl = document.querySelector('button');
+//const inputEl = document.querySelector('input');
+//const listEl = document.querySelector('ul');
+
+//function addGoal() {
+  //const enteredValue = inputEl.value;
+  //const listItemEl = document.createElement('li');
+  //listItemEl.textContent = enteredValue;
+  //listEl.appendChild(listItemEl);
+  //inputEl.value = '';
+//}
+
+//buttonEl.addEventListener('click', addGoal);
+ //40  
 
